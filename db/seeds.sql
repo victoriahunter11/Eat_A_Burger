41f69dc19f4996b1d47a14dcf6348215fc0663c5
Victoria Hunter <victoria.jane09@gmail.com>
2020-12-29 14:25:45 -0500

    updating seeds and name of webpage

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Molly');
-INSERT INTO cats (name) VALUES ('Charlie');
-INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
-INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
-INSERT INTO cats (name, sleepy) VALUES ('Smudge', true);
-INSERT INTO cats (name) VALUES ('Daisy');
+INSERT INTO burgers (name) VALUES ('cheeseburger');
+INSERT INTO burgers (name) VALUES ('whopper');
+INSERT INTO burgers (name) VALUES ('quarter pounder');
+INSERT INTO burgers (name) VALUES ('mcdonalds');
+INSERT INTO burgers (name) VALUES ('burger king');
+INSERT INTO burgers (name) VALUES ('cookout');
